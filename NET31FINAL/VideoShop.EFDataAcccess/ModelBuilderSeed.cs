9d06780324ed8a00cc83aa5439153dad99f8210c
whooaaapppp <jwarain@me.com>
2020-04-02 20:11:13 -0400

    submission partial

---
+++ NET31FINAL/VideoShop.EFDataAcccess/ModelBuilderSeed.cs
@@ -0,0 +1,53 @@
+﻿using VideoShop.Pocos;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoShop.EFDataAcccess
+{
+    public static class ModelBuilderSeed
+    {
+        public static void SeedData(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<VideoPoco>().HasData(
+                new VideoPoco
+                {
+                    Id = Guid.NewGuid(),
+                    VideoName = "The Witcher",
+                    ReleaseDate = new DateTime(2010, 04, 23),
+                    Borrower = new BorrowerPoco
+                    {
+                        Id = Guid.NewGuid(),
+                        BorrowerName = "Jessie Winston",
+                        DOB = new DateTime(1992, 09, 16)
+                    }
+                },
+                 new VideoPoco
+                 {
+                     Id = Guid.NewGuid(),
+                     VideoName = "Contagion",
+                     ReleaseDate = new DateTime(2011, 07, 23),
+                     Borrower = new BorrowerPoco
+                     {
+                         Id = Guid.NewGuid(),
+                         BorrowerName = "Hazel Jane",
+                         DOB = new DateTime(1992, 09, 16)
+                     }
+                 },
+                 new VideoPoco
+                 {
+                     Id = Guid.NewGuid(),
+                     VideoName = "The Last of Us",
+                     ReleaseDate = new DateTime(2020, 01, 23),
+                     Borrower = new BorrowerPoco
+                     {
+                         Id = Guid.NewGuid(),
+                         BorrowerName = "Chris Joe",
+                         DOB = new DateTime(1992, 09, 16)
+                     }
+                 }
+            );
+        }
+    }
+}
---
+++ NET31FINAL/VideoShop.EFDataAcccess/VideoShopContext.cs
@@ -0,0 +1,35 @@
+﻿using VideoShop.Pocos;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace VideoShop.EFDataAcccess
+{
+    public class VideoShopContext : DbContext
+    {
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            config.AddJsonFile(path, false);
+            var root = config.Build();
+            string _connStr = root.GetSection("ConnectionStrings").GetSection("DataConnection").Value;
+            optionsBuilder.UseSqlServer(_connStr);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<BorrowerPoco> BorrowerPocos { get; set; }
+        public DbSet<VideoPoco> VideoPocos { get; set; }
+    }
+}
